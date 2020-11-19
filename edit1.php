17e5106725bc4965972751624d0df7c1a1dc902b
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2020-11-20 03:59:50 +0700

    Add files via upload

---
+++ edit1.php
@@ -0,0 +1,16 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'lab13-itf.mysql.database.azure.com', 'ingkawat@lab13-itf', '087493765Zin', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$id = $_GET['ID'];
+$sql = "SELECT FROM guestbook WHERE id = $id";
+if (mysqli_query($conn, $sql)) {
+echo "Record deleted successfully";
+} else {
+echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
