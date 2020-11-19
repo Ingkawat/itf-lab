8094c746fe4a468d69e31faa3f166de5a2199197
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2020-11-20 03:15:35 +0700

    Update delete1.php

--- delete1.php
+++ delete1.php
@@ -5,7 +5,7 @@ if (mysqli_connect_errno($conn))
 {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$id = $_GET['ID'];
+$id = $_GET['id'];
 $sql = "DELETE FROM guestbook WHERE id = $id";
 if (mysqli_query($conn, $sql)) {
 echo "Record deleted successfully";
