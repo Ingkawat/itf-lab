b2cae237cdc7fd421ea8f6c7c64c4f71923aae75
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2020-11-20 18:32:09 +0700

    Update edit1.php

--- edit1.php
+++ edit1.php
@@ -1,3 +1,10 @@
+<div class="container">
+  <form action="edit1.php" method="post">
+        Name: <input class="form-control" type="text" name="name" /><br>
+        Comment: <input class="form-control" type="text" name="comment" /><br>
+        <input type="submit" />
+        </form>
+</div>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab13-itf.mysql.database.azure.com', 'ingkawat@lab13-itf', '087493765Zin', 'itflab', 3306);
@@ -6,9 +13,10 @@ if (mysqli_connect_errno($conn))
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 $id = $_GET['id'];
-echo $id;  
+$name = $_POST['name'];
+$comment= $_POST['comment'];  
 
-$sql = "UPDATE guestbook SET Comment='comment', Name='name' WHERE id=$id";
+$sql = "UPDATE guestbook SET Comment='$comment', Name='$name' WHERE id=$id";
 if (mysqli_query($conn, $sql)) {
 echo "Edit successfully";
 $query = mysqli_query($conn, $sql);
@@ -18,6 +26,3 @@ echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 mysqli_close($conn);
 ?>
 
-<div class="container">
-  <center><a href="show2.php"><input type="button" value="Show" class="btn btn-info" class="ex1"></a></center>
-</div>
